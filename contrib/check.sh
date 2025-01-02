4ea3b895ab73124f8eaec20c50b143e70b706f0c
Zowie <git@zowie.cx>
2025-01-02 20:57:27 +0100

    build: add targets and feature permutations to contrib/check.sh

--- contrib/check.sh
+++ contrib/check.sh
@@ -9,17 +9,41 @@ set -euo pipefail
 
 export RUSTFLAGS="-Dwarnings"
 
+HOST_TARGET="$(rustc --version --verbose | pcregrep -o1 'host: (.*)')"
+
+FEAT_PERMUTATIONS=(
+  "amqp-amqprs"
+  "amqp-lapin"
+)
+
+TARGETS=("x86_64-unknown-linux-gnu" "x86_64-unknown-linux-musl")
+
 echo "* auditing dependencies..."
 cargo audit
 
-echo "* running clippy..."
-cargo clippy
+for permutation in ${FEAT_PERMUTATIONS[@]}; do
+  echo "* running clippy ($permutation)..."
+  cargo clippy --no-default-features -F "$permutation" --quiet
+done
 
-echo "* running tests (x86-unknown-linux-musl)..."
-cross test --target=x86_64-unknown-linux-musl --release
+for target in ${TARGETS[@]}; do
+  # clean up the target/release folder otherwise some weird issues happen with GLIBC and serde
+  rm -rf target/release
 
-echo "* building binaries (x86-unknown-linux-musl)..."
-cross build --target=x86_64-unknown-linux-musl --release
+  if [[ "$target" = "$HOST_TARGET" ]]; then
+    rustBin=cargo
+  else
+    rustBin=cross
+  fi
+
+  for permutation in ${FEAT_PERMUTATIONS[@]}; do
+    echo "* building binaries ($target, $permutation)..."
+    $rustBin build --target=$target --no-default-features -F "$permutation" --release --quiet
+
+    echo "* running tests ($target, $permutation)..."
+    $rustBin test --target=$target --no-default-features -F "$permutation" --release --quiet
+  done
+done
 
 echo "* building containers..."
 docker compose --profile=full build
