0245e465c4f06e7515a29dccfbd3570254dc73ad
Zowie <git@zowie.cx>
2025-01-03 03:39:05 +0100

    refactor(scheduler): refactor the scheduler so we can actually enable/disable tasks, even when the scheduler isn't running yet

--- framework/src/scheduler.rs
+++ framework/src/scheduler.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, future::Future, pin::Pin};
 
 use async_cron_scheduler::{Job, JobId, Scheduler as CronScheduler};
 use chrono::Utc;
@@ -10,7 +10,8 @@ use crate::{
 
 pub struct Scheduler {
     job_map: HashMap<String, JobId>,
-    scheduler: Option<CronScheduler<Utc>>,
+    scheduler: CronScheduler<Utc>,
+    runner: Option<Pin<Box<dyn Future<Output = ()> + Send + Sync>>>,
 }
 
 impl Scheduler {
@@ -19,10 +20,41 @@ impl Scheduler {
         reason = "we might have constructor arguments in the future, having a Default implementation feels incorrect"
     )]
     pub fn new() -> Self {
+        let (scheduler, service) = CronScheduler::<Utc>::launch(tokio::time::sleep);
+
         Self {
             job_map: HashMap::new(),
-            scheduler: None,
+            scheduler,
+            runner: Some(Box::pin(service)),
         }
+    }
+
+    pub async fn enable_task<T: Clone + Send + Sync + 'static>(&mut self, ctx: Context<T>, task: TaskHandler<T>) {
+        let job = Job::<Utc>::cron_schedule(task.cron.clone());
+        let job_name = task.name.clone();
+        let job_id = self
+            .scheduler
+            .insert(job, move |_id| {
+                let job_ctx = ctx.clone();
+                let job_handler = task.clone();
+
+                tokio::spawn(async move {
+                    if let Err(err) = job_handler.run(TaskContext::from_context(job_ctx)).await
+                    {
+                        tracing::error!("error running task {}: {}", job_handler.name, err);
+                    };
+                });
+            })
+            .await;
+        self.job_map.insert(job_name, job_id);
+    }
+
+    pub async fn disable_task(&mut self, name: &str) {
+        let Some(job_id) = self.job_map.get(name) else {
+            return
+        };
+
+        self.scheduler.remove(*job_id).await;
     }
 
     pub async fn run<T: Clone + Send + Sync + 'static>(
@@ -30,30 +62,14 @@ impl Scheduler {
         ctx: Context<T>,
         tasks: Vec<&TaskHandler<T>>,
     ) -> tokio::task::JoinHandle<()> {
-        let (mut scheduler, sched_service) = CronScheduler::<Utc>::launch(tokio::time::sleep);
-
-        for task in tasks {
-            let loop_ctx = ctx.clone();
-            let loop_handler = task.clone();
-
-            let job = Job::<Utc>::cron_schedule(task.cron.clone());
-            let job_id = scheduler
-                .insert(job, move |_id| {
-                    let job_ctx = loop_ctx.clone();
-                    let job_handler = loop_handler.clone();
-
-                    tokio::spawn(async move {
-                        if let Err(err) = job_handler.run(TaskContext::from_context(job_ctx)).await
-                        {
-                            tracing::error!("error running task {}: {}", job_handler.name, err);
-                        };
-                    });
-                })
-                .await;
-            self.job_map.insert(task.name.clone(), job_id);
+        if self.runner.is_none() {
+            panic!("Scheduler::run called twice, scheduler is already running");
         }
 
-        self.scheduler = Some(scheduler);
-        tokio::spawn(sched_service)
+        for task in tasks {
+            self.enable_task(ctx.clone(), task.clone()).await
+        }
+
+        tokio::spawn(self.runner.take().unwrap())
     }
 }
